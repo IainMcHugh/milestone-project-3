0c09fd986325c03b3708bef2f481a4952d5dfc76
IainMcHugh <iamchugh@tcd.ie>
2021-03-14 14:52:51 +0000

    Getting page data with mock response data

--- README.md
+++ README.md
@@ -5,3 +5,41 @@ note: in env.py change key
 Need form for adding a new page
 
 construct edit url with unique website object id.
+
+```typescript
+type UserWebsite = {
+    website_id: websiteObjectId();
+    isOwner: boolean;
+}
+
+interface Users {
+  username: string;
+  email: string;
+  websites: UserWebsite[]
+}
+
+type Comments = {
+  username: string;
+  timestamp: Date;
+  value: string;
+  comment_type: 'COMMENT' | 'BUG' | 'UPDATE';
+};
+
+interface Websites {
+  objectId: objectId();
+  owners: string[];
+  title: string;
+  url: string;
+  description: string;
+  stars: number;
+  reviews: number;
+  last_update: string;
+  comments: Comments[];
+}
+```
+
+1. Sign up and Log in - (Create Read user document)
+2. As user, CRUD website - (Create Read Update Delete website/objectId for website stored against user, data stored in Website)
+3. As user, add comment - (push comment to Websites comment array)
+
+template inheritance - components
--- app.py
+++ app.py
@@ -17,10 +17,53 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_users')
-def get_users():
-    users = mongo.db.users.find()
-    return render_template('users.html', users=users)
+def index():
+    # user = mongo.db.users.find()
+    # return render_template('user.html', user=user)
+    websites = {'popular': [
+        {'url': 'test1', 'image': 'test_image1'},
+        {'url': 'test2', 'image': 'test_image2'}
+        ],
+        'recent': [
+        {'url': 'recent_test1', 'image': 'recent_test_image1'},
+        {'url': 'recent_test2', 'image': 'recent_test_image2'}
+        ],
+        'random': [
+        {'url': 'random_test1', 'image': 'random_test_image1'},
+        {'url': 'random_test2', 'image': 'random_test_image2'}
+        ],
+        }
+    return render_template('index.html', websites=websites)
+
+
+@app.route('/siteDetails')
+def siteDetails():
+    website = {
+        'owners': ['IainMcHugh'],
+        'title': 'WebApp Store',
+        'url': 'https://www.webappstore.com',
+        'description': 'This is a description of the site.',
+        'stars': 4,
+        'reviews': 100,
+        'last_update': 'This is the most recent update description.',
+        'comments': [
+            {
+                'username': 'Comment_bob_123',
+                'timestamp': '1970-05-25, 14:22:32:1394',
+                'value': 'I really like this website!',
+                'stars': 4,
+                'comment_type': 'COMMENT',
+            },
+            {
+                'username': 'Comment_jane_456',
+                'timestamp': '1970-05-26, 14:22:46:1394',
+                'value': 'I found this bug I think should be fixed!',
+                'stars': 3,
+                'comment_type': 'BUG',
+            },
+        ]
+    }
+    return render_template('siteDetails.html', website=website)
 
 
 if __name__ == '__main__':
