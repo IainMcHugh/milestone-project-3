788a7fc2fbb92d671659879f83df1416526ec11e
IainMcHugh <iamchugh@tcd.ie>
2021-03-27 14:50:09 +0000

    return categories for homepage

--- app.py
+++ app.py
@@ -17,31 +17,49 @@ app.secret_key = os.environ.get('SECRET_KEY')
 mongo = PyMongo(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     # websites = list(mongo.db.websites.find())
-    websites = {'popular': [
-        {'url': 'test1', 'image': ''},
-        {'url': 'test2', 'image': ''},
-        {'url': 'test2', 'image': ''},
-        {'url': 'test2', 'image': ''},
-        {'url': 'test2', 'image': ''},
-        ],
-        'recent': [
-        {'url': 'recent_test1', 'image': ''},
-        {'url': 'recent_test2', 'image': ''},
-        {'url': 'recent_test1', 'image': ''},
-        {'url': 'recent_test2', 'image': ''},
-        {'url': 'recent_test1', 'image': ''}
-        ],
-        'random': [
-        {'url': 'random_test1', 'image': ''},
-        {'url': 'random_test2', 'image': ''},
-        {'url': 'random_test1', 'image': ''},
-        {'url': 'random_test2', 'image': ''},
-        {'url': 'random_test1', 'image': ''}
-        ],
-        }
+    # websites = {'popular': [
+    #     {'url': 'test1', 'image': ''},
+    #     {'url': 'test2', 'image': ''},
+    #     {'url': 'test2', 'image': ''},
+    #     {'url': 'test2', 'image': ''},
+    #     {'url': 'test2', 'image': ''},
+    # ],
+    #     'recent': [
+    #     {'url': 'recent_test1', 'image': ''},
+    #     {'url': 'recent_test2', 'image': ''},
+    #     {'url': 'recent_test1', 'image': ''},
+    #     {'url': 'recent_test2', 'image': ''},
+    #     {'url': 'recent_test1', 'image': ''}
+    # ],
+    #     'random': [
+    #     {'url': 'random_test1', 'image': ''},
+    #     {'url': 'random_test2', 'image': ''},
+    #     {'url': 'random_test1', 'image': ''},
+    #     {'url': 'random_test2', 'image': ''},
+    #     {'url': 'random_test1', 'image': ''}
+    # ],
+    # }
+    # get popular websites
+    websites_popular = [
+        x for x in mongo.db.websites.find().sort("stars", -1).limit(5)]
+    # get recently added websites
+    websites_recent = [
+        x for x in mongo.db.websites.find().sort("_id", -1).limit(5)]
+    # get random websites
+    websites_random = [x for x in mongo.db.websites.aggregate(
+        [{"$sample": {"size": 5}}]
+    )]
+    websites = {
+        'popular': websites_popular,
+        'recent': websites_recent,
+        'random': websites_random
+    }
+    print(websites_popular)
+    print(websites_recent)
+    print(websites_random)
     return render_template('index.html', websites=websites)
 
 
@@ -50,23 +68,6 @@ def siteDetails(websiteid):
     print(websiteid)
     website = mongo.db.websites.find_one({"_id": ObjectId(websiteid)})
     return render_template('siteDetails.html', website=website)
-    # websitet = {
-    #     'comments': [
-    #         {
-    #             'username': 'bob_123',
-    #             'timestamp': '2 days ago',
-    #             'value': 'I really like this website!',
-    #             'stars': 4,
-    #             'comment_type': 'COMMENT',
-    #         },
-    #         {
-    #             'username': 'jane_456',
-    #             'timestamp': '3 days ago',
-    #             'value': 'I found this bug I think should be fixed!',
-    #             'stars': 3,
-    #             'comment_type': 'BUG',
-    #         },
-    #     ]
 
 
 @app.route('/user/<username>', methods=['GET', 'POST'])
@@ -74,38 +75,21 @@ def user(username):
     if session["user"]:
         user_data = mongo.db.users.find_one({"username": session["user"]})
         user_websites = []
-        for website in user_data["websites"]:
-            user_websites.append(mongo.db.websites.find_one({"_id": ObjectId(website)}))
+        if "websites" in user_data:
+            for website in user_data["websites"]:
+                user_websites.append(mongo.db.websites.find_one(
+                    {"_id": ObjectId(website)}))
+                print(user_websites)
+
+        if not user_websites:
+            flash(
+                'Click the "ADD" button to publish your first website!', 'info')
         return render_template(
-                'user.html', username=user_data["username"],
-                user_data=user_data,
-                user_websites=user_websites)
+            'user.html', username=user_data["username"],
+            user_data=user_data,
+            user_websites=user_websites)
     return redirect(url_for("index"))
-    # user = {
-    #     'username': 'Iain_McHugh',
-    #     'email': 'iamchugh@tcd.ie',
-    #     'websites': [
-    #         {
-    #             'id': '123456',
-    #             'name': 'My website',
-    #             'url': 'https://www.google.com',
-    #             'isOwner': True
-    #         },
-    #         {
-    #             'id': '123456',
-    #             'name': 'My second website',
-    #             'url': 'https://www.google.com',
-    #             'isOwner': True
-    #         },
-    #         {
-    #             'id': '123456',
-    #             'name': 'My third website',
-    #             'url': 'https://www.google.com',
-    #             'isOwner': False
-    #         },
-    #     ]
-    # }
-    
+
 
 @app.route("/createSite", methods=["GET", "POST"])
 def createSite():
@@ -123,7 +107,7 @@ def createSite():
         website = mongo.db.websites.insert_one(site)
         mongo.db.users.find_one_and_update(
             {'username': session["user"]},
-            { "$push": {"websites": website.inserted_id}},
+            {"$push": {"websites": website.inserted_id}},
             upsert=True)
         flash("Your website was published successfully", 'success')
         # redirect to siteDetails with inserted_id
@@ -137,16 +121,17 @@ def updateSite(websiteid):
     if request.method == 'POST':
         website = mongo.db.websites.find_one_and_update(
             {"_id": ObjectId(websiteid)},
-            { "$set": {
-            "title": request.form.get('site_name'),
-            "url": request.form.get('site_url'),
-            "owner": session["user"],
-            "description": request.form.get('site_description'),
+            {"$set": {
+                "title": request.form.get('site_name'),
+                "url": request.form.get('site_url'),
+                "owner": session["user"],
+                "description": request.form.get('site_description'),
             }})
-        updated_website = mongo.db.websites.find_one({"_id": ObjectId(websiteid)})
+        updated_website = mongo.db.websites.find_one(
+            {"_id": ObjectId(websiteid)})
         flash("Your website was updated successfully", "success")
         return render_template('siteDetails.html', website=updated_website)
-    
+
     website = mongo.db.websites.find_one({"_id": ObjectId(websiteid)})
     return render_template('updateSite.html', website=website)
 
@@ -155,9 +140,9 @@ def updateSite(websiteid):
 def deleteSite(websiteid):
     # need to delete from websites and user website list
     mongo.db.users.find_one_and_update(
-            {'username': session["user"]},
-            { "$pull": {"websites": ObjectId(websiteid)}},
-            upsert=True)
+        {'username': session["user"]},
+        {"$pull": {"websites": ObjectId(websiteid)}},
+        upsert=True)
     mongo.db.websites.remove({"_id": ObjectId(websiteid)})
     flash('Website was successfully removed', 'success')
     return redirect(url_for('user', username=session['user']))
@@ -172,16 +157,16 @@ def signup():
         if pwd != re_pwd:
             flash('Passwords do not match.', 'error')
             return redirect(url_for('index'))
-        
+
         existing_username = mongo.db.users.find_one(
             {'username': request.form.get('username').lower()})
         existing_email = mongo.db.users.find_one(
             {'email': request.form.get('email').lower()})
-        
+
         if existing_username or existing_email:
             flash("Username or email already exists.", 'error')
             return redirect(url_for('index'))
-        
+
         register = {
             "username": request.form.get('username').lower(),
             "email": request.form.get('email').lower(),
@@ -191,8 +176,8 @@ def signup():
         mongo.db.users.insert_one(register)
 
         session["user"] = request.form.get('username').lower()
-        flash('Welcome ' + request.form.get('username') + 
-        ', you are now logged in', 'success')
+        flash('Welcome ' + request.form.get('username') +
+              ', you are now logged in', 'success')
         return redirect(url_for('user', username=session["user"]))
 
 
@@ -204,10 +189,11 @@ def login():
 
         if existing_user:
             if check_password_hash(
-                existing_user["password"], request.form.get('password')):
-                    session["user"] = existing_user["username"].lower()
-                    flash('Welcome ' + existing_user["username"] + ', you are now logged in', 'success')
-                    return redirect(url_for('user', username=session["user"]))
+                    existing_user["password"], request.form.get('password')):
+                session["user"] = existing_user["username"].lower()
+                flash(
+                    'Welcome ' + existing_user["username"] + ', you are now logged in', 'success')
+                return redirect(url_for('user', username=session["user"]))
             else:
                 flash('Invalid email or password', 'error')
                 return redirect(url_for('index'))
@@ -225,5 +211,5 @@ def logout():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True) # change to False in production
+            port=int(os.environ.get('PORT')),
+            debug=True)  # change to False in production
